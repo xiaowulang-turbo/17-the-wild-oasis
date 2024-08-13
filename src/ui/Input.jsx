93c5f93745ae9b862724d8f120da6e543af3c7e1
xiaowulang-turbo <2728795232@qq.com>
2024-08-13 20:49:21 +0800

    feat: create input ui styled-components

---
+++ src/ui/Input.jsx
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+const Input = styled.input`
+  border: 1px solid var(--color-grey-300);
+  background-color: #fff;
+  border-radius: 4px;
+  padding: 10px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+`;
+
+export default Input;
