abec885398a84dcbd511264e86f5c359a903cfeb
xiaowu <2728795232@qq.com>
2024-12-22 13:02:54 +0800

    feat(cabins): add Empty component for no cabin data

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -7,12 +7,15 @@ import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
   const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  if (!cabins) return <Empty resource={"cabins"} />;
 
   // 1. Filter
   const filterValue = searchParams.get("discount") || "all";
