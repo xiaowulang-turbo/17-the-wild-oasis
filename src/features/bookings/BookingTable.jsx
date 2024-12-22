7c6b037b8f5c0adaa897abfa7280e39dcc49f9d8
xiaowu <2728795232@qq.com>
2024-12-22 13:01:27 +0800

    feat(bookings): add useBookings hook and handle empty state

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -1,9 +1,13 @@
 import BookingRow from "./BookingRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import useBookings from "./useBookings";
+import Empty from "../../ui/Empty";
 
 function BookingTable() {
-  const bookings = [];
+  const { bookings } = useBookings();
+
+  if (!bookings) return <Empty resource={"bookings"} />;
 
   return (
     <Menus>
