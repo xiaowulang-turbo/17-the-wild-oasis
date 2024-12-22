49085ee34c7291fc8231e520e7a77499735d1963
xiaowu <2728795232@qq.com>
2024-12-22 17:37:31 +0800

    feat(ui/Sidebar): add Uploader component to Sidebar

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
+import Uploader from "../data/Uploader";
 
 const StyleSidebar = styled.div`
   background-color: var(--color-grey-0);
@@ -18,6 +19,8 @@ function Sidebar() {
     <StyleSidebar>
       <Logo />
       <MainNav />
+
+      <Uploader />
     </StyleSidebar>
   );
 }
