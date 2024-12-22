070c1264cc40966ea624fbb505faf6cf644ef1a4
xiaowu <2728795232@qq.com>
2024-12-22 17:37:52 +0800

    feat(bookings): add BookingTableOperations component

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,13 +1,14 @@
 import BookingTable from "../features/bookings/BookingTable";
+import BookingTableOperations from "../features/bookings/BookingTableOperations";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Bookings() {
   return (
     <>
-      <Row type="horizontal">
-        <Heading as="h1">All bookings</Heading>
-        <p>Filter / Sort</p>
+      <Row type='horizontal'>
+        <Heading as='h1'>All bookings</Heading>
+        <BookingTableOperations />
       </Row>
 
       <BookingTable />
