6b909d7ad2044b31cea4afcfb788d3012ae720d4
xiaowu <2728795232@qq.com>
2024-12-22 17:38:07 +0800

    feat(bookings): add status filter to bookings query

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export default function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : {
+          field: "status",
+          value: filterValue,
+        };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    // dependencies, when it changes, the query is refetched
+    queryKey: ["bookings", filter],
+    // receives a function that returns the data
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isLoading, bookings, error };
