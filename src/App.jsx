d8eee6e54001c9378a00689291a63704a114402e
xiaowu <2728795232@qq.com>
2024-12-24 13:21:31 +0800

    feat: add BookingDetail route and import

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Users from "./pages/Users";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
 import { Toaster } from "react-hot-toast";
+import BookingDetail from "./features/bookings/BookingDetail";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -35,6 +36,7 @@ function App() {
               <Route index element={<Navigate replace to="dashboard" />} />
               <Route path="dashboard" element={<DashBoard />} />
               <Route path="bookings" element={<Bookings />} />
+              <Route path="bookings/:id" element={<BookingDetail />} />
               <Route path="cabins" element={<Cabins />} />
               <Route path="account" element={<Account />} />
               <Route path="settings" element={<Settings />} />
