75fb96b524e308e9d7aaaaad8cf545a31a2bbd8b
xiaowu <2728795232@qq.com>
2024-12-25 12:00:23 +0800

    feat(check-in-out): add useCheckin hook for booking check-in functionality

---
+++ src/features/check-in-out/useCheckin.js
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { updateBooking } from "../../services/apiBookings";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export function useCheckin() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  // mutate && isLoading, we cannot say mutation
+  const { mutate: checkin, isLoading: isCheckingIn } = useMutation({
+    mutationFn: (bookingId) =>
+      updateBooking(bookingId, { status: "checked-in", isPaid: true }),
+
+    onSuccess: (data) => {
+      toast.success(`Booking ${data.id} successfully checked in `);
+      queryClient.invalidateQueries({ active: true });
+      navigate("/");
+    },
+
+    onError: () => toast.error("There was an error while checking in"),
+  });
+
+  return { checkin, isCheckingIn };
+}
