f04ac03f22be7455c59505b9f41e14fbf5853a97
xiaowu <2728795232@qq.com>
2024-12-26 19:47:33 +0800

    feat(authentication): add useLogin hook for login functionality

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,24 @@
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import loginApi from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+
+export default function useLogin() {
+  const navigate = useNavigate();
+
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+
+    onSuccess: () => {
+      toast.success("Login successful");
+      navigate("/dashboard");
+    },
+
+    onError: (error) => {
+      console.log("ERROR", error);
+      toast.error("Provide email or password are incorrect");
+    },
+  });
+
+  return { login, isLoading };
+}
