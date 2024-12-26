35d830b58e6d0d27d6cad9418c9bd5de0a67a303
xiaowu <2728795232@qq.com>
2024-12-26 10:40:16 +0800

    refactor(bookings): Add default empty objects to guests and cabins destructuring

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -58,8 +58,8 @@ function BookingRow({
     numGuests,
     totalPrice,
     status,
-    guests: { fullName: guestName, email },
-    cabins: { name: cabinName },
+    guests: { fullName: guestName, email } = {},
+    cabins: { name: cabinName } = {},
   },
 }) {
   const navigate = useNavigate();
