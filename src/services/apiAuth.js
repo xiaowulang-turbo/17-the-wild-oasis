e2f91a2901b8c41d58b1f658233b0377b2cfcb71
xiaowu <2728795232@qq.com>
2024-12-26 19:47:26 +0800

    feat(apiAuth): add authentication API using Supabase

---
+++ src/services/apiAuth.js
@@ -0,0 +1,12 @@
+import supabase from "./supabase";
+
+export default async function apiAuth({ email, password }) {
+  let { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
+    password: password,
+  });
+
+  if (error) throw new Error(error.message);
+
+  return data;
+}
