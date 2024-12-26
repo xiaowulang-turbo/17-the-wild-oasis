3ccb844112001d8be82588e97e676f3a255d54e4
xiaowu <2728795232@qq.com>
2024-12-26 19:48:05 +0800

    feat(ui): add FormRowVertical component for vertical form layout

---
+++ src/ui/FormRowVertical.jsx
@@ -0,0 +1,34 @@
+import { styled } from "styled-components";
+import PropTypes from "prop-types";
+
+FormRowVertical.propTypes = {
+  label: PropTypes.string,
+  error: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+const StyledFormRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  padding: 1.2rem 0;
+`;
+
+const Label = styled.label`
+  font-weight: 500;
+`;
+
+const Error = styled.span`
+  font-size: 1.4rem;
+  color: var(--color-red-700);
+`;
+
+export default function FormRowVertical({ label, error, children }) {
+  return (
+    <StyledFormRow>
+      {label && <Label htmlFor={children.props.id}>{label}</Label>}
+      {children}
+      {error && <Error>{error}</Error>}
+    </StyledFormRow>
+  );
+}
