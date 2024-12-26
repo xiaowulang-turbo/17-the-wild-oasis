fda89cb080faeacaa78f97018b68d29a5d3586b2
xiaowu <2728795232@qq.com>
2024-12-26 21:20:58 +0800

    feat(authentication): add queryClient to handle user data on login success

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,16 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 
 export default function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
 
-    onSuccess: () => {
+    onSuccess: (user) => {
       toast.success("Login successful");
+      queryClient.setQueriesData(["user"], user);
       navigate("/dashboard");
     },
 
