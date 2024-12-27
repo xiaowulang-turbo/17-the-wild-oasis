cc97aab2f24b0f120c7ba7c725f469a6ca0be74a
xiaowu <2728795232@qq.com>
2024-12-27 19:33:55 +0800

    feat(authentication): add signup functionality with loading state

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,14 +3,23 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import useSignup from "./useSignup";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
   const { errors } = formState;
 
+  const { signup, isLoading } = useSignup();
+
   const onSubmit = (data) => {
+    signup(data, {
+      // this should receive a callback function
+      onSettled: () => {
+        reset();
+      },
+    });
     console.log(data);
   };
 
@@ -74,10 +83,10 @@ function SignupForm() {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button variation="secondary" type="reset" disabled={isLoading}>
           Cancel
         </Button>
-        <Button>Create new user</Button>
+        <Button disabled={isLoading}>Create new user</Button>
       </FormRow>
     </Form>
   );
