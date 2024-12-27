1f5dcb34994710d2e504433e6711fbe5048e9e1b
xiaowu <2728795232@qq.com>
2024-12-27 09:57:07 +0800

    feat(authentication): add useLogout hook for user logout functionality

---
+++ src/features/authentication/useLogout.js
@@ -0,0 +1,22 @@
+import { useMutation } from "@tanstack/react-query";
+import { logout as logoutApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export default function useLogout() {
+  const navigate = useNavigate();
+
+  const { mutate: logout, isLoading } = useMutation({
+    mutationFn: logoutApi,
+    onSuccess: () => {
+      navigate("/login", { replace: true });
+      toast.success("Logout successful");
+    },
+
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  return { logout, isLoading };
+}
