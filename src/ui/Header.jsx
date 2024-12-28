f57c2ae9ba326dbcc650e5eec3bdb98aac8dba5f
xiaowu <2728795232@qq.com>
2024-12-28 13:38:21 +0800

    feat(ui/Header): Add UserAvatar and HeaderMenu components

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,16 +1,23 @@
 import styled from "styled-components";
-import Logout from "./Logout";
+import HeaderMenu from "./HeaderMenu";
+import UserAvatar from "../features/authentication/UserAvatar";
 
 const StyleHeader = styled.div`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
+
+  display: flex;
+  gap: 2.4rem;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 function Header() {
   return (
     <StyleHeader>
-      <Logout />
+      <UserAvatar />
+      <HeaderMenu />
     </StyleHeader>
   );
 }
