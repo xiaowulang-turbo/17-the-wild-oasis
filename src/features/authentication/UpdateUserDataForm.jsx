017466a880a8bd82a5517d32d0e06f8a8704331d
xiaowu <2728795232@qq.com>
2024-12-28 15:30:39 +0800

    feat(authentication): add user data update functionality

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 
 import Button from "../../ui/Button";
@@ -6,7 +7,8 @@ import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
-import { useUser } from "./useUser";
+import useUser from "./useUser";
+import useUpdateUser from "./useUpdateUser";
 
 function UpdateUserDataForm() {
   // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
@@ -20,8 +22,25 @@ function UpdateUserDataForm() {
   const [fullName, setFullName] = useState(currentFullName);
   const [avatar, setAvatar] = useState(null);
 
+  const { updateUser, isUpdating } = useUpdateUser();
+
   function handleSubmit(e) {
     e.preventDefault();
+    if (!fullName) return;
+    updateUser(
+      { fullName, avatar },
+      {
+        onSuccess: () => {
+          setAvatar(null);
+          e.target.reset();
+        },
+      }
+    );
+  }
+
+  function handleCancel(e) {
+    setFullName(currentFullName);
+    setAvatar(null);
   }
 
   return (
@@ -33,22 +52,33 @@ function UpdateUserDataForm() {
         <Input
           type="text"
           value={fullName}
+          disabled={isUpdating}
           onChange={(e) => setFullName(e.target.value)}
           id="fullName"
         />
       </FormRow>
+
       <FormRow label="Avatar image">
         <FileInput
           id="avatar"
           accept="image/*"
+          disabled={isUpdating}
           onChange={(e) => setAvatar(e.target.files[0])}
         />
       </FormRow>
+
       <FormRow>
-        <Button type="reset" variation="secondary">
+        <Button
+          type="reset"
+          variation="secondary"
+          disabled={isUpdating}
+          onClick={handleCancel}
+        >
           Cancel
         </Button>
-        <Button>Update account</Button>
+        <Button type="submit" disabled={isUpdating}>
+          Update account
+        </Button>
       </FormRow>
     </Form>
   );
