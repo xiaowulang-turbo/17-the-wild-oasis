93b46dc1818262a2a1b5d59fd55e022818641047
xiaowu <2728795232@qq.com>
2024-12-28 13:38:18 +0800

    feat(ui): add HeaderMenu component with user and logout buttons

---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,27 @@
+import { HiOutlineUser } from "react-icons/hi2";
+import ButtonIcon from "./ButtonIcon";
+import Logout from "./Logout";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+
+export default function HeaderMenu() {
+  const navigate = useNavigate();
+
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate("/account")}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+}
