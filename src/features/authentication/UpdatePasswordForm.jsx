c760fd01a1464e7912589b4fd618bbfca496e270
xiaowu <2728795232@qq.com>
2024-12-28 15:30:44 +0800

    fix(authentication): clarify password validation message in UpdatePasswordForm

--- src/features/authentication/UpdatePasswordForm.jsx
+++ src/features/authentication/UpdatePasswordForm.jsx
@@ -4,7 +4,7 @@ import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
-import { useUpdateUser } from "./useUpdateUser";
+import useUpdateUser from "./useUpdateUser";
 
 function UpdatePasswordForm() {
   const { register, handleSubmit, formState, getValues, reset } = useForm();
@@ -31,7 +31,7 @@ function UpdatePasswordForm() {
             required: "This field is required",
             minLength: {
               value: 8,
-              message: "Password needs a minimum of 8 characters",
+              message: "New password needs a minimum of 8 characters",
             },
           })}
         />
@@ -53,6 +53,7 @@ function UpdatePasswordForm() {
           })}
         />
       </FormRow>
+
       <FormRow>
         <Button onClick={reset} type="reset" variation="secondary">
           Cancel
