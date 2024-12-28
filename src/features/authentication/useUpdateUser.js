562e033584d8d936642c8e5cb9336bd7b00bc60e
xiaowu <2728795232@qq.com>
2024-12-28 15:30:22 +0800

    feat(authentication): add useUpdateUser hook for user account updates

---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateCurrentUser } from "../../services/apiAuth";
+import toast from "react-hot-toast";
+
+export default function useUpdateUser() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateUser, isLoading: isUpdating } = useMutation({
+    mutationFn: updateCurrentUser,
+    onSuccess: ({ user }) => {
+      toast.success("User account successfully updated");
+      queryClient.setQueryData(["user"], user);
+      queryClient.invalidateQueries(["user"]);
+    },
+
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+  return { updateUser, isUpdating };
+}
