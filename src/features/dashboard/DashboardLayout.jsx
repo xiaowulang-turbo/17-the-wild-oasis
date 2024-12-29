24c2298194bf4e61df6cd2362de110103b420a03
xiaowu <2728795232@qq.com>
2024-12-29 12:37:07 +0800

    feat(dashboard): add recent bookings and stays with loading spinner

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import useRecentBookings from "./useRecentBookings";
+import useRecentStays from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,21 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+export default function DashboardLayout() {
+  const { bookings, isLoading: isLoadingBookings } = useRecentBookings();
+  const { stays, isLoading: isLoadingStays, confirmedStays } = useRecentStays();
+
+  console.log(bookings, stays, confirmedStays);
+
+  if (isLoadingBookings || isLoadingStays) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today&apos;s activity</div>
+      <div>Chart stay durations</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
