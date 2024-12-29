f818b026c5c6075eece4babdc00ee3ca141f9320
xiaowu <2728795232@qq.com>
2024-12-29 12:36:52 +0800

    feat(dashboard): add useRecentBookings hook for fetching recent bookings

---
+++ src/features/dashboard/useRecentBookings.js
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBookingsAfterDate } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
+import { subDays } from "date-fns";
+
+export default function useRecentBookings() {
+  const [searchParams] = useSearchParams();
+
+  const numDays = searchParams.get("last")
+    ? parseInt(searchParams.get("last"))
+    : 7;
+
+  console.log(searchParams.get("last"), numDays);
+
+  const queryDate = subDays(new Date(), numDays).toISOString();
+
+  const { data: bookings, isLoading } = useQuery({
+    queryKey: ["bookings", `last-${numDays}`],
+    queryFn: () => getBookingsAfterDate(queryDate),
+  });
+
+  return {
+    bookings,
+    isLoading,
+  };
+}
