7db99965c9a0ab3d10fe43ce0025e3bb51221e58
xiaowu <2728795232@qq.com>
2024-12-29 12:36:58 +0800

    feat(dashboard): add useRecentStays hook for fetching recent stays

---
+++ src/features/dashboard/useRecentStays.js
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import { getStaysAfterDate } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
+import { subDays } from "date-fns";
+
+export default function useRecentStays() {
+  const [searchParams] = useSearchParams();
+
+  const numDays = searchParams.get("last")
+    ? parseInt(searchParams.get("last"))
+    : 7;
+
+  const queryDate = subDays(new Date(), numDays).toISOString();
+
+  const { data: stays, isLoading } = useQuery({
+    queryKey: ["stays", `last-${numDays}`],
+    queryFn: () => getStaysAfterDate(queryDate),
+  });
+
+  const confirmedStays = stays?.filter(
+    (stay) => stay.status === "checked-in" || stay.status === "checked-out"
+  );
+
+  return {
+    stays,
+    isLoading,
+    confirmedStays,
+  };
+}
